484b63f957bd9ada085aefc39901e39ea56b8618
赵龙 <zhao.long@zhongjunguoji.com>
2020-02-24 14:14:07 +0800

    2020年2月24日14:14:05

---
+++ 基础/34.numpy.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+# 用于一维创建列表
+print(np.array([1,2,3])) 
+# 用元组创建
+print(np.array((1,2,3)))
+
+# 多维数组
+print(np.array([[1,2,3],[4,5,6]]))
+
+
